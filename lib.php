c2f8537ffd34a7762a2458bdaf14383b6163a4eb
Roberto <io_robertob@hotmail.com>
2019-11-19 12:43:20 +0200

    start work to get metadata from dummy file included in description in the poster settings, now adding logging

--- lib.php
+++ lib.php
@@ -172,8 +172,9 @@ function poster_page_type_list($pagetype, $parentcontext, $currentcontext) {
 
         
         file_print('INSTANCE ID',TRUE);
-        // file_print($context->cm);
-        file_print($context->cmid);
+        file_print($context->instanceid);
+        file_print($context->instance);
+
 
         // // TODO: here to implement the autopopulation of metadata, from files' metadata
         // $activity_module      = $DB->get_record('course_modules',array('id' =>$context         ->instanceid)); // get the module where the course is the current course
