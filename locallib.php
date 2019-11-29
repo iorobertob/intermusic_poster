bfaaf51e234ff27b7d3239c2387ef7f39ff83f90
Roberto <io_robertob@hotmail.com>
2019-11-29 17:52:32 +0200

    added debug log

--- locallib.php
+++ locallib.php
@@ -293,7 +293,7 @@ function get_metadata_from_api($resourcespace_id, $moduleinstance, $list_metadat
             if ($row["title"] === $list_metadata[$i])
             {
                 $new_list_metadata[$i] = $row["value"];
-                poster_print("VALUE: ".$row["value"]);
+                poster_print("VALUE[".$i."] = ".$row["value"]);
             }
         }
     } 
