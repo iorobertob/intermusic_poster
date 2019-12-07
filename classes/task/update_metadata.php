67cbb9bcee39e8e900b493528b1673857e3f8cd8
Roberto <io_robertob@hotmail.com>
2019-12-07 13:08:08 +0200

    add print line

--- classes/task/update_metadata.php
+++ classes/task/update_metadata.php
@@ -28,7 +28,7 @@ defined('MOODLE_INTERNAL') || die();
 require_once($CFG->dirroot . '/mod/poster/lib.php');
 require_once($CFG->dirroot . '/mod/poster/locallib.php');
 require_once("$CFG->libdir/resourcelib.php");
-require_once($CFG->dirroot . '/lib/accesslib.php');
+require($CFG->dirroot . '/lib/accesslib.php');
 
 /**
  * The main scheduled task for the forum.
