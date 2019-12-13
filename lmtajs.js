c0a80ef17adb01cf3c16fde12145673a222838db
Roberto <io_robertob@hotmail.com>
2019-12-13 11:18:07 +0200

    remove useles line that was intended to recognize the audio playback and detect markers in audio files

--- lmtajs.js
+++ lmtajs.js
@@ -1,14 +1,14 @@
 
-var audioPlayer = document.getElementById("audiofile");
+// var audioPlayer = document.getElementById("audiofile");
 
-audioPlayer.addEventListener("timeupdate", function(e)
-{
-	syncData.forEach(function(element, index, array)
-	{
-	    if( audioPlayer.currentTime >= 0.5 && audioPlayer.currentTime <= 1.0 )
-	        console.log("time stamp");
-	});
-});
+// audioPlayer.addEventListener("timeupdate", function(e)
+// {
+// 	syncData.forEach(function(element, index, array)
+// 	{
+// 	    if( audioPlayer.currentTime >= 0.5 && audioPlayer.currentTime <= 1.0 )
+// 	        console.log("time stamp");
+// 	});
+// });
 
 // Draggable elements by class name, from their title element
 var cardClassName  = "card-block";
