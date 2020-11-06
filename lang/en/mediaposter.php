e5a64759eacb8adccef09747609f83d4d38996b1
Roberto <iorobertob@gmail.com>
2020-11-06 23:32:44 +0200

    added new mediaposter lang file

---
+++ lang/en/mediaposter.php
@@ -0,0 +1,72 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings of the Media Poster activity module are defined here.
+ *
+ * @package     mod_mediaposter
+ * @category    string
+ * @copyright   2015 David Mudrak <david@moodle.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'Media Poster';
+$string['meta_label_1'] = 'Metadata Field #1';
+$string['meta_label_2'] = 'Metadata Field #2';
+$string['meta_label_3'] = 'Metadata Field #3';
+$string['meta_label_4'] = 'Metadata Field #4';
+$string['meta_label_5'] = 'Metadata Field #5';
+$string['meta_label_6'] = 'Metadata Field #6';
+$string['meta_label_7'] = 'Metadata Field #7';
+$string['meta_label_file'] = 'Metadata File';
+$string['meta1'] = 'Metadata Field #1:';
+$string['meta2'] = 'Metadata Field #2:';
+$string['meta3'] = 'Metadata Field #3:';
+$string['meta4'] = 'Metadata Field #4:';
+$string['meta5'] = 'Metadata Field #5:';
+$string['meta6'] = 'Metadata Field #6:';
+$string['meta7'] = 'Metadata Field #7:';
+$string['meta_value']  = 'Value';
+$string['meta_title']  = 'Title';
+$string['modulename_help'] = 'The mediaposter module enables a teacher to make a page out of blocks (such as the HTML block, comments, calendar or RSS feed block), dragging and dropping them to the preferred location.
+
+You can use the mediaposter
+
+* As a space for your students to present results of their work.
+* As a course dashboard, collating useful elements for students on a single page and keeping the main course page clean and clear of side blocks.';
+$string['modulenameplural']     	= 'Media Posters';
+$string['page-mod-mediaposter-view'] 	= 'Media Poster module main page';
+$string['pluginadministration'] 	= 'Media Poster administration';
+$string['pluginname'] 				= 'Media Poster';
+$string['mediaposter:addinstance'] 		= 'Add a new mediaposter';
+$string['mediaposter:view'] 				= 'View mediaposter';
+$string['mediapostername'] 				= 'Title';
+$string['overwrite'] 				= 'Overwrite Intermusic metadata with custom fields.';
+$string['mediaposterauthor'] 			= 'Composer';
+$string['mediapostersurtitle'] 			= 'Surtitle';
+$string['mediaposternumbering'] 			= 'Numbering';
+$string['mediaposterlanguage'] 			= 'Language';
+$string['autopopulate'] 			= 'Autopopulate from childern \'File\' blocks';
+$string['privacy:metadata'] 		= 'Media Poster does not store any personal data';
+$string['showdescriptionview'] 		= 'Display description on view page';
+$string['showdescriptionview_help'] = 'If enabled, the description above will be displayed on the mediaposter page.';
+$string['shownameview'] 			= 'Display name on view page';
+$string['shownameview_help'] 		= 'If enabled, the mediaposter name will be displayed as heading on the mediaposter page.';
+$string['metadatafile']    = 'Metadata file';
+$string['resourcecontent'] = 'Files and subfolders';
+$string['metadata_task'] = 'Update Metadata';
