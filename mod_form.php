e26eac10d0a927d1d11c5be596f9d25d4f37c03a
Roberto <iorobertob@gmail.com>
2020-11-08 00:51:01 +0200

    remove overwrite checkbox

--- mod_form.php
+++ mod_form.php
@@ -51,10 +51,7 @@ class mod_mposter_mod_form extends moodleform_mod {
         $mform->addRule('name', null, 'required', null, 'client');
         $mform->addRule('name', get_string('maximumchars', 'core', 255), 'maxlength', 255, 'client');
 
-
         ///////////////////////////////////// METADATA FIELDS ////////////////////////////////
-
-        $mform->addElement('advcheckbox', 'overwrite', get_string('overwrite', 'mod_mposter'), '', array('group' => 1), array(0, 1));
 
         // Metadata Field #1
         $mform->addElement('header', 'meta_label_1', get_string('meta_label_1', 'mod_mposter'));
